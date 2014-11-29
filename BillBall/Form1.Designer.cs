8fe2cd399cc5bed6f598975def7817433f59d2b8
Brandon Forster <brandonforster@gmail.com>
2014-11-29 15:56:33 -0500

    Refactor, run cole's parse on button press

--- BillBall/Form1.Designer.cs
+++ BillBall/Form1.Designer.cs
@@ -29,31 +29,67 @@
         private void InitializeComponent()
         {
             this.dataBox = new System.Windows.Forms.RichTextBox();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.sbmtBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // dataBox
             // 
-            this.dataBox.Location = new System.Drawing.Point(12, 12);
-            this.dataBox.Name = "richTextBox1";
-            this.dataBox.Size = new System.Drawing.Size(596, 407);
+            this.dataBox.Location = new System.Drawing.Point(12, 38);
+            this.dataBox.Name = "dataBox";
+            this.dataBox.Size = new System.Drawing.Size(596, 381);
             this.dataBox.TabIndex = 0;
             this.dataBox.Text = "";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(98, 12);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(429, 20);
+            this.inputBox.TabIndex = 1;
+            // 
+            // sbmtBtn
+            // 
+            this.sbmtBtn.Location = new System.Drawing.Point(533, 9);
+            this.sbmtBtn.Name = "sbmtBtn";
+            this.sbmtBtn.Size = new System.Drawing.Size(75, 23);
+            this.sbmtBtn.TabIndex = 2;
+            this.sbmtBtn.Text = "Submit";
+            this.sbmtBtn.UseVisualStyleBackColor = true;
+            this.sbmtBtn.Click += new System.EventHandler(this.sbmtBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Term to Search";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(620, 431);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sbmtBtn);
+            this.Controls.Add(this.inputBox);
             this.Controls.Add(this.dataBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.RichTextBox dataBox;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button sbmtBtn;
+        private System.Windows.Forms.Label label1;
 
     }
 }
